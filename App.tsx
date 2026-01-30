2a211e126050b9f301dabae1d32f99f6b08c8b6b
Akshay R K <akshayrk@Akshays-Mac-mini.local>
2026-01-30 22:59:11 +0530

    Create a button with Pressable

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this ?",
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting."),
+          style: "destructive"
+        },
+        {
+          text: "Cancel",
+          style: "cancel"
+        }
+      ]
+    )
+  }
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDelete} activeOpacity={0.8}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -19,13 +39,27 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
     borderBottomWidth: 1,
     borderBottomColor: theme.colorCerulean,
-    paddingHorizontal: 8,
-    paddingVertical: 16
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200"
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: 'uppercase',
+    letterSpacing: 1,
   }
 });
